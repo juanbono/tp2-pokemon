b3d41810ba758b59878c212ff7137a7600672596
Lionel Yoshida <liozzs@gmail.com>
2015-06-07 10:18:20 -0300

    Tipos es un trait que tiene un metodo mataA

--- tp2-pokemon/src/utn/frba/pokemon/Tipo.scala
+++ tp2-pokemon/src/utn/frba/pokemon/Tipo.scala
@@ -1,5 +1,16 @@
 package utn.frba.pokemon
 
-class Tipo {
-
+//si esta estructura funciona implementar todos los tipos
+trait Tipo {
+  def mataA : List[Tipo] 
 }
+
+object Fuego extends Tipo {
+  def mataA : List[Tipo] = List(Planta)
+}
+
+object Planta extends Tipo {
+  def mataA : List[Tipo] = List()
+}
+
+
