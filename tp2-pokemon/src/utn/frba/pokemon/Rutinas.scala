fd0e6b1a35aa4dbd7bf0377685ce7356cf5c46da
Lionel Yoshida <liozzs@gmail.com>
2015-06-11 18:53:05 -0300

    Implementacion BASICA de Rutinas y Analizador con criterios + test * Nueva clase para el ResultadoRutina, no se si es la mejor forma, pero es para saber si una rutina termino bien o no, esto hay que mejorarlo. * Ver de guardar los criterios en algun lugar * Ver de unificar todos los tests

--- tp2-pokemon/src/utn/frba/pokemon/Rutinas.scala
+++ tp2-pokemon/src/utn/frba/pokemon/Rutinas.scala
@@ -1,17 +1,31 @@
 package utn.frba.pokemon
 
-class Rutina(nombre : String, actividades : Actividad*) {
-  def ejecutar(pokemon : Pokemon) : Estado = {
-    Simulador.entrenar(pokemon, actividades: _*)
+class Rutina(val nombre : String, val actividades : Actividad*) {
+  def ejecutar(pokemon : Pokemon) : ResultadoRutina = {
+    ResultadoRutina(rutina = this, estado = Simulador.entrenar(pokemon, actividades: _*))
   }
 }
 
+case class ResultadoRutina(val rutina : Rutina, val estado : Estado) {
+  val pokemon = estado.pokemon
+  
+  def termino : Boolean = {    
+    // mejorar.
+    // devuelve si la rutina termino bien. Ver que se hace si termina en estado DOrmido, Envenenado, etc...
+    // ver cual es la mejor forma de saber si pudo termina la rutina bien?
+    estado match {
+      case EstadoNormal(_) => true
+      case _ => false
+    }
+  }
+}
 
-class AnalizadorRutinas(criterio : (Rutina => Int), rutinas : Rutina*) {
+class AnalizadorRutinas(criterio : (ResultadoRutina, ResultadoRutina) => Boolean, rutinas : Rutina*) {
   def mejorRutina(pokemon : Pokemon) : String = {
-    val resultadosRutinas = rutinas.map { rutina => rutina.ejecutar(pokemon) }
+    val resultadosRutinas = rutinas.map { rutina => rutina.ejecutar(pokemon) }.filter { resultado => resultado.termino }
+    val resultadosOrdenados = resultadosRutinas.sortWith(criterio)
     
-    
-    "nombre mejor rutina"
+    //mejorar en caso de q no haya ninguna.
+    resultadosOrdenados.head.rutina.nombre
   }
 }
