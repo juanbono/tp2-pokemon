8c15acac8f83a029a1a355eea86e090ceca802be
Lionel Yoshida <liozzs@gmail.com>
2015-06-24 21:52:25 -0300

    Refactor de los tipos.

--- tp2-pokemon/src/utn/frba/pokemon/Tipo.scala
+++ tp2-pokemon/src/utn/frba/pokemon/Tipo.scala
@@ -1,66 +1,22 @@
 package utn.frba.pokemon
 
-// Tipos que puede tener una especie de pokemon.
 trait Tipo {
-  def mataA(tipo : Tipo) : Boolean 
+  def tiposPeores : List[Tipo]
+  def mataA(tipo : Tipo) : Boolean = tiposPeores.contains(tipo)
 }
 
-case object Fuego extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Planta, Hielo, Bicho).contains(tipo)
-}
-
-case object Agua extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Fuego, Tierra, Roca).contains(tipo)
-}
-
-case object Planta extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Agua, Tierra, Roca).contains(tipo)
-}
-
-case object Tierra extends Tipo{
-  def mataA(tipo : Tipo) : Boolean = List(Fuego, Electrico, Veneno, Roca).contains(tipo)
-}
-
-case object Hielo extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Planta, Tierra, Volador, Dragon).contains(tipo)
-}
-
-case object Roca extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Fuego, Hielo, Volador, Bicho).contains(tipo)
-}
-
-case object Electrico extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Agua, Volador).contains(tipo)
-}
-
-case object Psiquico extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Pelea, Veneno).contains(tipo)
-}
-
-case object Pelea extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Normal, Hielo, Roca).contains(tipo)
-}
-
-case object Fantasma extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Psiquico, Fantasma).contains(tipo)
-}
-
-case object Volador extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Planta, Pelea, Bicho).contains(tipo)
-}
-
-case object Bicho extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Planta, Psiquico).contains(tipo)
-}
-
-case object Veneno extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Planta).contains(tipo)
-}
-
-case object Dragon extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List(Dragon).contains(tipo)
-}
-
-case object Normal extends Tipo {
-  def mataA(tipo : Tipo) : Boolean = List().contains(tipo)
-}
+object Fuego extends Tipo {def tiposPeores = List(Planta, Hielo, Bicho)}
+object Agua extends Tipo {def tiposPeores = List(Fuego, Tierra, Roca)}
+object Planta extends Tipo {def tiposPeores = List(Agua, Tierra, Roca)}
+object Tierra extends Tipo {def tiposPeores = List(Fuego, Electrico, Veneno, Roca)}
+object Hielo extends Tipo {def tiposPeores = List(Planta, Tierra, Volador, Dragon)}
+object Roca extends Tipo {def tiposPeores = List(Fuego, Hielo, Volador, Bicho)}
+object Electrico extends Tipo {def tiposPeores = List(Agua, Volador)}
+object Psiquico extends Tipo {def tiposPeores = List(Pelea, Veneno)}
+object Pelea extends Tipo {def tiposPeores = List(Normal, Hielo, Roca)}
+object Fantasma extends Tipo {def tiposPeores = List(Fantasma, Psiquico)}
+object Volador extends Tipo {def tiposPeores = List(Planta, Pelea, Bicho)}
+object Bicho extends Tipo {def tiposPeores = List(Planta, Psiquico)}
+object Veneno extends Tipo {def tiposPeores : List[Tipo] = List(Planta, Hielo, Bicho)}
+object Dragon extends Tipo {def tiposPeores = List(Dragon)}
+object Normal extends Tipo {def tiposPeores = List()}
